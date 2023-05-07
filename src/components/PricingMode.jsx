37f22cca27424981e74e138782dabba4e36e35c1
Christhino <mintsaniaina.christhino@gmail.com>
2023-05-07 05:42:55 +0300

    fix card

--- src/components/PricingMode.jsx
+++ src/components/PricingMode.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import { motion } from "framer-motion";
 
-import "react-vertical-timeline-component/style.min.css";
 
 import { styles } from "../styles";
 import { experiences, experiencesEnglish } from "../constants";
