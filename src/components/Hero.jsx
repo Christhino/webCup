e7d771a60759f04c061d7502e4d3b3558163169f
henimbola <henimbola@gmail.com>
2023-05-07 05:36:59 +0300

    fix header hero problems

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,31 +9,32 @@ const Hero = () => {
     <section className={`relative w-full h-screen mx-auto`}>
       <div
         className={`absolute inset-0 max-w-7xl mx-auto sm:px-0 px-6 flex flex-row items-center gap-5`}
-        // className={`absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
-          <div className="flex flex-col justify-center items-center mt-5">
+        <div className="flex gap-5">
+          <div className="flex flex-col items-center mt-5">
             <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
             <div className="w-1 sm:h-80 h-40 violet-gradient" />
           </div>
 
           <div>
             <h1 className={`${styles.heroHeadText} text-white titre`}>
-            <FormattedMessage id="header-part-1"/>   <span className="text-[#915EFF]"><FormattedMessage id="header-part-2" /></span>
+              <FormattedMessage id="header-part-1" />   <span className="text-[#915EFF]"><FormattedMessage id="header-part-2" /></span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100 intro`}>
-            <FormattedMessage id="header-description-part-1" />  <br className="sm:block hidden" /> <FormattedMessage id="header-description-part-2" />
+              <FormattedMessage id="header-description-part-1" />  <br className="sm:block hidden" /> <FormattedMessage id="header-description-part-2" />
             </p>
           </div>
-          <div className="flex flex-col justify-center items-center mt-5">
-            <img
-                src={github}
-                alt="github"
-                className="image-AI w-[1200px] h-full object-contain"
-            />
-          </div>
+        </div>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <img
+            src={github}
+            alt="github"
+            className="image-AI w-[1200px] h-full object-contain"
+          />
+        </div>
       </div>
-      
-      
+
+
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
