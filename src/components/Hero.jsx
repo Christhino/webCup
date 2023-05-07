8d37e9f97f32e7d3a9638b8b1bef0d1ef007ae19
henimbola <henimbola@gmail.com>
2023-05-07 05:16:23 +0300

    fix design issues in the hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,8 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 max-w-7xl mx-auto sm:px-0 px-6 flex flex-row items-center gap-5`}
+        // className={`absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
           <div className="flex flex-col justify-center items-center mt-5">
             <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
